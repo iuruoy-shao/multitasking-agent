adca02fd4b40f4facca8d0eeca76be731f0f2482
iuruoy-shao <youruishao115022@gmail.com>
2025-07-08 13:06:48 -0700

    add tasks

--- src/coding_agents/crew.py
+++ src/coding_agents/crew.py
@@ -13,17 +13,17 @@ from typing import List
 class CodingAgents(Agent):
     """
     """
-    
+
     agents: List[BaseAgent]
     tasks: List[Task]
-    
+
     @agent
     def manager(self) -> Agent:
         return BaseAgent(
             config = self.agents_config['manager'],
             verbose = True,
         )
-    
+
     @agent
     def file_reader(self) -> Agent:
         return BaseAgent(
@@ -31,7 +31,7 @@ class CodingAgents(Agent):
             verbose = True,
             tools = [DirectoryReadTool(), FileReadTool()],
         )
-    
+
     @agent
     def engineer(self) -> Agent:
         return BaseAgent(
@@ -39,26 +39,58 @@ class CodingAgents(Agent):
             verbose = True,
             tools = [FileReadTool(), FileWriterTool(), MakeBranch(), Commit()],
         )
-        
+
     @agent
     def git_manager(self) -> Agent:
         return BaseAgent(
             config = self.agents_config['git_manager'],
             verbose = True,
-            tools = ,
+            # tools = ,
         )
-        
+
+    @task
+    def validate_response(self) -> Task:
+        return Task(
+            config = self.tasks_config['validate_response'],
+        )
+
+    @task
+    def split_tasks(self) -> Task:
+        return Task(
+            config = self.tasks_config['split_tasks'],
+        )
+
     @task
     def read_system(self) -> Task:
         return Task(
             config = self.tasks_config['read_system'],
             async_execution = True,
         )
-        
+
     @task
     def make_changes(self) -> Task:
         return Task(
             config = self.tasks_config['make_changes'],
+            context = [self.read_system()],
+        )
+
+    @task
+    def create_branch(self) -> Task:
+        return Task(
+            config = self.tasks_config['create_branch'],
+        )
+
+    @task
+    def commit_changes(self) -> Task:
+        return Task(
+            config = self.tasks_config['commit_changes'],
+        )
+
+    @task
+    def merge_pr(self) -> Task:
+        return Task(
+            config = self.tasks_config['merge_pr'],
+            context = [], # Assuming context is an empty list based on tasks.yaml
         )
     
     @crew
