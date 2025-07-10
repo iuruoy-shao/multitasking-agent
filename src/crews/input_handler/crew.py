21e3bd1e10247983f9e3021a2def45f0551e7763
iuruoy-shao <youruishao115022@gmail.com>
2025-07-10 14:21:47 -0700

    fix input parsing conditional

--- src/crews/input_handler/crew.py
+++ src/crews/input_handler/crew.py
@@ -5,16 +5,20 @@ from crewai_tools import (
     DirectoryReadTool,
     FileReadTool,
 )
+from crewai.tasks.conditional_task import ConditionalTask
 from crewai.tasks.task_output import TaskOutput
 from pydantic import BaseModel
 from typing import List, Text
 
+class ValidationOutput(BaseModel):
+    valid: bool
+    response: Text
+
 class TaskList(BaseModel):
-    items: List[Text]
+    tasks: List[Text]
     
 def valid_input(output: TaskOutput) -> bool:
-    print(output.raw)
-    return output.raw
+    return output.pydantic.valid
 
 @CrewBase
 class Manager():
@@ -37,15 +41,15 @@ class Manager():
     def validate_response(self) -> Task:
         return Task(
             config = self.tasks_config['validate_response'],
-            human_input = True
+            output_pydantic = ValidationOutput,
         )
     
     @task
-    def split_tasks(self) -> Task:
+    def split_tasks(self) -> ConditionalTask:
         return Task(
             config = self.tasks_config['split_tasks'],
             output_pydantic = TaskList,
-            conditional = valid_input,
+            condition = valid_input,
         )    
     
     @crew
--- src/crews/main.py
+++ src/crews/main.py
@@ -3,11 +3,10 @@ from input_handler.crew import Manager
 from output_handler.crew import GitManager
 
 def run(user_input: str):
-    task_list = Manager().crew().kickoff(inputs={
+    Manager().crew().kickoff(inputs={
         'user_input': user_input,
         'dir': '/Users/Yourui/Documents/Caltech'
     })
-    print(task_list)
     
 if __name__ == "__main__":
     run("what files are in my directory?")
