17c60406aa34898d15c73843c9e489b22b69a3bf
iuruoy-shao <youruishao115022@gmail.com>
2025-07-14 14:16:34 -0700

    fix directory issue

--- src/crews/main.py
+++ src/crews/main.py
@@ -2,19 +2,20 @@ from engineers.crew import Engineer
 from input_handler.crew import Manager, TaskList
 from output_handler.crew import GitManager
 import asyncio
+import os
 
 async def run(user_input: str):
     directory = '/Users/Yourui/Documents/test'
     
+    os.chdir(directory)
+    
     output = Manager().crew().kickoff(inputs={
         'user_input': user_input,
-        'dir': directory
     }).pydantic
     
     if isinstance(output, TaskList): # If task splitting was completed, i.e., the user input was valid.
         await Engineer().crew().kickoff_for_each_async([{
             'task': task,
-            'dir': directory
         } for task in output.tasks])
     else:
         return output.response
@@ -22,4 +23,4 @@ async def run(user_input: str):
     # kickoff_for_each_async()
     
 if __name__ == "__main__":
-    asyncio.run(run("make a new, empty file called test.md in a folder called 'hello_world'"))
+    asyncio.run(run("make a new, empty file called test.md in a folder called 'hello_world'. Create a second file called 'test2.md' in the root, and write 'hello world' in it."))
