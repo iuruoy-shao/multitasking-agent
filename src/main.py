a9b4681e7bc9650bd6bf9daaf8d1a0e0626a3154
iuruoy-shao <youruishao115022@gmail.com>
2025-07-18 22:29:24 -0700

    fixed add to .gitignore

--- src/main.py
+++ src/main.py
@@ -11,22 +11,30 @@ def launch_engineer(task: str, queue: Queue):
     summary = Engineer().crew().kickoff(inputs={'task': task}).pydantic.response
     cprint(f"✓ Task completed: {task}, {summary.replace('\n', ' ')[:50]}...", 'green')
     queue.put(summary)
-
-def run(user_input: str, directory: str):
-    os.chdir(directory)
-
-    # ensure repository is initialized
-    if not os.path.exists(f'{directory}/.git'):
+    
+def git_init(directory: str):
+    if not os.path.exists(f'{directory}/.git'): # creates a git repository if it doesn't exist
         repo = git.Repo.init(directory)
         repo.git.add(A=True)
         repo.index.commit('Initial commit')
         cprint(f"✓ Initialized git repository in {directory}", 'green')
+    
+    gitignore_path = f'{directory}/.gitignore'
+    if os.path.isfile(gitignore_path):
+        with open(gitignore_path, 'r') as f:
+            if '.temp' not in f.read():
+                with open(gitignore_path, 'a') as f:
+                    f.write('\n# Ignore temporary worktrees\n')
+                    f.write('.temp\n')
+    else:
+        with open(gitignore_path, 'w') as f:
+            f.write('# Ignore temporary worktrees\n')
+            f.write('.temp\n')
 
-    with open(f'{directory}/.gitignore', 'a+') as f:
-        if '.temp' not in f.read():
-            f.write('\n# Ignore temporary worktrees\n')
-            f.write('.temp/\n')
-
+def run(user_input: str, directory: str):
+    os.chdir(directory)
+    git_init(directory) # ensure git is initialized
+    
     output = Manager().crew().kickoff(inputs={
         'user_input': user_input,
     }).pydantic
